4f9f0552e6ff39e0a1714d6242449c4674ec0dd7
Tony Thomas <tonythomas504@gmail.com>
2021-01-12 20:10:25 -0500

    heroku server deployment message

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.DB_CONNECTION_STRING, {
+const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
